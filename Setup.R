79c466731847aea463a023cf3c2da256400b3710
jinjinglow <jinjing.low@hotmail.com>
2025-11-06 22:09:42 +0800

    Updated Setup.R 6Nov2025

--- Setup.R
+++ Setup.R
@@ -6,5 +6,14 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("DESeq2") #Do note that DESeq2 may take a while to run if this is the first time
 # install.packages("ggplot2")
 # install.packages("matrixStats")
+# install.packages("pheatmap")
 library(DESeq2) 
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# If there are any issues with the existing packages, uninstall and install them again
+#remove.packages("DESeq2")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("DESeq2") #Do note that DESeq2 may take a while to run if this is the first time
 # ------------------------------------------------------------------------------
